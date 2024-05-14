cc2c48ec3d68de5cfc5b9bd89b296ac44a504b41
Meiado <gabrielmeiado@hotmail.com>
2024-05-14 20:59:11 -0300

    remoção de método inexistente

--- scripts/indexCtrl.js
+++ scripts/indexCtrl.js
@@ -1,39 +1,35 @@
 const register = async () => {
     const email = document.querySelector('#emailCadastro').value;
-    if(IsEmail(email)) {
-      const senha = document.querySelector('#senhaCadastro').value;
-      const cpf = document.querySelector('#cpfCadastro').value;
-      let usuario = {
-        email: email,
-        senha: senha,
-        cpf: cpf
-      };
-      try {
-        const loginResponse = await fetch('http://localhost:8080/access/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(usuario),
-        })
-        const token = await loginResponse.text();
-        console.log(token);
-    
-        localStorage.setItem('token', token);
+    const senha = document.querySelector('#senhaCadastro').value;
+    const cpf = document.querySelector('#cpfCadastro').value;
+    let usuario = {
+      email: email,
+      senha: senha,
+      cpf: cpf
+    };
+    try {
+      const loginResponse = await fetch('http://localhost:8080/access/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+      })
+      const token = await loginResponse.text();
+      console.log(token);
+  
+      localStorage.setItem('token', token);
 
-        const accessResponse = await fetch('http://localhost:8080/access?token=' + token, {
-          method: 'GET',
-        })
-        const access = parseInt(await accessResponse.text());
-        localStorage.setItem('access', access); 
-        redirect(access);
+      const accessResponse = await fetch('http://localhost:8080/access?token=' + token, {
+        method: 'GET',
+      })
+      const access = parseInt(await accessResponse.text());
+      localStorage.setItem('access', access); 
+      redirect(access);
 
-      } catch (err) {
-        console.error(err);
-      }
-  }
-  else 
-    document.querySelector('#mensagem').innerHTML = 'Favor inserir um email com formato válido';
+    } catch (err) {
+      console.error(err);
+    }
 }
 
 const verificaSessao = () => {
@@ -73,33 +69,29 @@ const redirect = (access) => {
 
 const login = async () => {
     const email = document.querySelector('#emailLogin').value;
-    if(IsEmail(email)) {
-      const senha = document.querySelector('#senhaLogin').value;
-      let usuario = { email: email, senha: senha };
-      try {
-          const res = await fetch('http://localhost:8080/access/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(usuario),
-          })
-          const token = await res.text();
-          localStorage.setItem('token', token);
+    const senha = document.querySelector('#senhaLogin').value;
+    let usuario = { email: email, senha: senha };
+    try {
+        const res = await fetch('http://localhost:8080/access/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+        })
+        const token = await res.text();
+        localStorage.setItem('token', token);
 
-          const accessResponse = await fetch('http://localhost:8080/access?token=' + token, {
-          method: 'GET',
-          })
-          const access = parseInt(await accessResponse.text());
-          localStorage.setItem('access', access); 
-          redirect(access);
+        const accessResponse = await fetch('http://localhost:8080/access?token=' + token, {
+        method: 'GET',
+        })
+        const access = parseInt(await accessResponse.text());
+        localStorage.setItem('access', access); 
+        redirect(access);
 
-      } catch (err) {
-          console.error(err);
-      }
+    } catch (err) {
+        console.error(err);
     }
-    else
-      document.querySelector('#mensagem').innerHTML = 'Favor inserir um email com formato válido';
 }
 
 window.onload = verificaSessao();
