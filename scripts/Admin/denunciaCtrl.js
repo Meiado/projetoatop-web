5974694b002bbc69027fd2e62e629454be9a8d64
Meiado <gabrielmeiado@hotmail.com>
2024-05-21 20:15:30 -0300

    atualização visual das tabelas

--- scripts/Admin/denunciaCtrl.js
+++ scripts/Admin/denunciaCtrl.js
@@ -38,8 +38,10 @@ const loadDenuncias = async () => {
                     <td>${denuncia.titulo}</td>
                     <td>${new Date(denuncia.data).toLocaleDateString('pt-BR')}</td>
                     <td>${denuncia.urgencia}</td> 
-                    <td name="feedback" id="feedback${denuncia.id}">Sem feedback</td>
-                    <td><button id="button${denuncia.id}" type="button" class ="btn btn-primary rounded-pill pt-1 pb-1">Alterar feedback</button></td>
+                    <td name="feedback" id="feedback${denuncia.id}">Feedback não fornecido</td>
+                    <td><button id="button${denuncia.id}" type="button" class ="btn btn-primary rounded-pill pt-1 pb-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+                  </svg></button></td>
                     <td id="imagem${denuncia.id}">Sem imagem</td>
                 `                
     }
